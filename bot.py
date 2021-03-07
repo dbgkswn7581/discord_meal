e396b0e0edd9ced93248d044ffc1f88d7c42e96f
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2021-03-08 00:30:57 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -327,7 +327,7 @@ async def meal(ctx):
     now = time.localtime()
 
     embed = discord.Embed(title = "Time",
-    description = "%04d/%02d/%02d %02d:%02d:%02d" %(now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec), color = discord.Color.orange()
+    description = "%04d/%02d/%02d %02d:%02d:%02d" %(int(now.tm_year+9), now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec), color = discord.Color.orange()
     )
     await ctx.send(embed=embed)
 
